04ed27523ef5f5e3dba27a7093a9ece3c29f4106
Raiele Leite <raiele.leite@hotmail.com>
2026-04-19 16:38:44 -0300

    Production config

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,20 @@
 import knex from "knex";
 import type { Knex } from "knex";
-import 'dotenv/config'
+import "dotenv/config";
+import { env } from "./env/index.js";
 
-if(!process.env.DATABASE_URL){
-  throw new Error('DATABASE_URL env not found.')
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env not found.");
 }
-
+// bd em dev --> sqlite3
 export const config: Knex.Config = {
-  client: "sqlite3",
-  connection: {
-    filename: process.env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite3"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
